62c2f51f0cc02c4edaa763bb7d725c2495c4d3ce
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-30 23:57:27 +0530

    Add POST and GET route with search by title

---
+++ controllers/movieControllers.js
@@ -0,0 +1,75 @@
+const Movie = require('../models/Movie');
+
+const addReview = async (req, res) => {
+    try {
+        const { title, genre, rating, review } = req.body
+
+        const movie = new Movie({ title, genre, rating, review });
+        await movie.save();
+
+        return res.status(201).json({
+            message: 'Review added successfully',
+            movie
+        });
+    }
+    catch(error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+const getReviewsAndSearchByTitle = async (req, res) => {
+    try {
+        const { title } = req.query;
+        
+        if(!title) {
+            const movies = await Movie.find();
+            if(movies.length === 0) {
+                return res.status(404).json({
+                    message: 'There are no movies here'
+                });
+            }
+            return res.status(200).json(movies);
+        }
+
+        const filteredMovies = await Movie.find({
+            title: { $regex: title, $options: 'i'}
+        });
+        if(filteredMovies.length === 0) {
+            return res.status(404).json({
+                message: `No reviews for the movie: ${title}`
+            });
+        }
+    }
+    catch(error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+const getReviewById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findById(id)
+
+        if(!movie) {
+            return res.status(404).json({
+                message: `No movie-review with ID: ${id}`
+            });
+        }
+
+        return res.status(200).json(movie);
+    }
+    catch(error) {
+        if(error.name === 'CastError') {
+            return res.status(400).json({
+                message: 'Invalid ID'
+            });
+        }
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
