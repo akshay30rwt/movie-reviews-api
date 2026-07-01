89325d52ea2bd42595e4154417017285c22f497f
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-01 12:38:22 +0530

    Connect MongoDB and run API

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const movieRoutes = require('./routes/movieRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use = express.json()
+app.use = ('/reviews', movieRoutes);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log('Connection failed', error)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
